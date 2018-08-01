33fdd63dea359b8eef2e04c5eab1c85b72704b70
cbryant <cbryant@SMC-CBRYANT-IM.local>
2018-08-01 14:54:37 -0700

    added ability to read trivia directory and json files

--- server/db/actions/trivia_question_add.js
+++ server/db/actions/trivia_question_add.js
@@ -3,7 +3,7 @@ require('module-alias/register');
 
 const { promisify } = require('util');
 const fs = require('fs');
-const { Questions } = require('@models');
+const { Questions, QuestionChoices } = require('@models');
 
 
 const readFileAsync = promisify(fs.readFile);
@@ -13,40 +13,32 @@ const readDirAsync = promisify(fs.readdir);
 let triviaCategories;
 let triviaFiles = {};
 let triviaRoot = './server/db/data/trivia_questions/';
-let testRoot = './server/db/data/trivia_questions/test';
+let triviaQuestions = [];
 
 ( async function () {
   try {
-    // triviaCategories = await readDirAsync(triviaRoot);
+    triviaCategories = await readDirAsync(triviaRoot);
 
-    // for (const category of triviaCategories) {
-    //   triviaFiles[category] = await readDirAsync(`${triviaRoot}/${category}`)
-    // }
-
-    triviaTestCategories = await readDirAsync(testRoot);
-
-    // let vals = JSON.parse( await readFileAsync('./server/db/data/trivia_questions/test/testQuestions.json'))
-    // debugger
-    // for ()
-
-
+    for (const category of triviaCategories) {
+      triviaFiles[category] = await readDirAsync(`${triviaRoot}/${category}`)
+    }
+    for (triviaCategory in triviaFiles) {
+      for ( const file of triviaFiles[triviaCategory]) {
+        const { results: trivia } = JSON.parse( await readFileAsync(`${triviaRoot}/${triviaCategory}/${file}`));
+        triviaQuestions.push(...trivia);
+      }
+    }
     debugger
-  } catch(err) {
-    console.log(err);
-    debugger
-  }
-})();
 
-( async function () {
-  try {
-    // questionFile = questionDir.forEach()
+    for (const trivia of triviaQuestions) {
+      Questions.create()
+    }
+
     
-    // JSON.parse( await readFileAsync(`./server/db/data/trivia_questions/test/testQuestions.json`));
-    // debugger
   } catch(err) {
     console.log(err);
     debugger
   }
 })();
 
-// debugger
+debugger
